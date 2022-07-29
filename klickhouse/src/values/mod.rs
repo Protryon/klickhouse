b11095bcaab1efe54a0f2d9cac4a562218745db2
Protryon <max.bruce12@gmail.com>
2022-07-29 09:51:59 -0700

    add guess_type function

--- klickhouse/src/values/mod.rs
+++ klickhouse/src/values/mod.rs
@@ -108,4 +108,44 @@ impl Value {
     pub fn from_value<T: ToSql>(value: T) -> Result<Self> {
         value.to_sql()
     }
+
+    /// Guesses a [`Type`] from the value, may not correspond to actual column type in Clickhouse
+    pub fn guess_type(&self) -> Type {
+        match self {
+            Value::Int8(_) => Type::Int8,
+            Value::Int16(_) => Type::Int16,
+            Value::Int32(_) => Type::Int32,
+            Value::Int64(_) => Type::Int64,
+            Value::Int128(_) => Type::Int128,
+            Value::Int256(_) => Type::Int256,
+            Value::UInt8(_) => Type::UInt8,
+            Value::UInt16(_) => Type::UInt16,
+            Value::UInt32(_) => Type::UInt32,
+            Value::UInt64(_) => Type::UInt64,
+            Value::UInt128(_) => Type::UInt128,
+            Value::UInt256(_) => Type::UInt256,
+            Value::Float32(_) => Type::Float32,
+            Value::Float64(_) => Type::Float64,
+            Value::Decimal32(p, _) => Type::Decimal32(*p),
+            Value::Decimal64(p, _) => Type::Decimal64(*p),
+            Value::Decimal128(p, _) => Type::Decimal128(*p),
+            Value::Decimal256(p, _) => Type::Decimal256(*p),
+            Value::String(_) => Type::String,
+            Value::Uuid(_) => Type::Uuid,
+            Value::Date(_) => Type::Date,
+            Value::DateTime(time) => Type::DateTime(time.0),
+            Value::DateTime64(tz, p, _) => Type::DateTime64(*p, *tz),
+            Value::Enum8(_) => unimplemented!(),
+            Value::Enum16(_) => unimplemented!(),
+            Value::Array(x) => Type::Array(Box::new(x.first().map(|x| x.guess_type()).unwrap_or(Type::String))),
+            Value::Tuple(values) => Type::Tuple(values.iter().map(|x| x.guess_type()).collect()),
+            Value::Null => Type::Nullable(Box::new(Type::String)),
+            Value::Map(k, v) => Type::Map(
+                Box::new(k.first().map(|x| x.guess_type()).unwrap_or(Type::String)),
+                Box::new(v.first().map(|x| x.guess_type()).unwrap_or(Type::String)),
+            ),
+            Value::Ipv4(_) => Type::Ipv4,
+            Value::Ipv6(_) => Type::Ipv6,
+        }
+    }
 }
