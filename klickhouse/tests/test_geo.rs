101d4b7367222cb517cbfa01950d7351d1faaad5
cpg314 <44120267+cpg314@users.noreply.github.com>
2023-12-24 11:35:11 +0100

    geo: Add an integration test with a complex multipolygon

--- klickhouse/tests/test_geo.rs
+++ klickhouse/tests/test_geo.rs
@@ -61,9 +61,43 @@ async fn test_client() {
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
     let items2 = client
-        // String directly.
         .query_collect::<Row>("SELECT * FROM test_geo")
         .await
         .unwrap();
     assert_eq!(items, items2);
+}
+
+#[derive(Clone, PartialEq, Debug, klickhouse::Row)]
+struct RowWkt {
+    multipolygon: MultiPolygon,
+}
+
+#[cfg(feature = "geo-types")]
+#[tokio::test]
+async fn test_client_wkt() {
+    let client = super::get_client().await;
+
+    super::prepare_table("test_geo_wkt", "multipolygon MultiPolygon", &client).await;
+    let multipolygon: geo_types::MultiPolygon = geo_types::wkt! {
+        // Example from https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry
+        MULTIPOLYGON (((40.0 40.0, 20.0 45.0, 45.0 30.0, 40.0 40.0)),
+                      ((20.0 35.0, 10.0 30.0, 10.0 10.0, 30.0 5.0, 45.0 20.0, 20.0 35.0),
+                       (30.0 20.0, 20.0 15.0, 20.0 25.0, 30. 20.0)))
+    };
+    let row = RowWkt {
+        multipolygon: MultiPolygon::from(multipolygon),
+    };
+
+    client
+        .insert_native_block("INSERT INTO test_geo_wkt FORMAT Native", vec![row.clone()])
+        .await
+        .unwrap();
+
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+    let row2 = client
+        .query_one::<RowWkt>("SELECT * FROM test_geo_wkt")
+        .await
+        .unwrap();
+    assert_eq!(row2, row);
 }
