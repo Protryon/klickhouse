9db5bc14b805e41941fab3aaa39342aaeeeaa1a0
cpg314 <44120267+cpg314@users.noreply.github.com>
2023-12-24 11:35:11 +0100

    Make the array serialization and deserialization methods more generic
    This is to later allow efficiently (de)serializing geo values without having to deconstruct Values.

--- klickhouse/src/types/deserialize/array.rs
+++ klickhouse/src/types/deserialize/array.rs
@@ -4,22 +4,42 @@ use crate::{io::ClickhouseRead, values::Value, Result};
 
 use super::{Deserializer, DeserializerState, Type};
 
+/// Trait to allow reading `Item`s and packing them into a `Value::*`.
+pub trait ArrayDeserializerGeneric {
+    type Item;
+    /// The type of the items, e.g. [Value]
+    fn inner_type(type_: &Type) -> &Type;
+    /// Mapping from items to the return Value, e.g. simply `Vec<Value> -> Value::Array(items)`.
+    fn inner_value(items: Vec<Self::Item>) -> Value;
+    /// Conversion between the [Value] read and the items, e.g. simply the identity.
+    fn item_mapping(value: Value) -> Self::Item;
+}
+
+/// Simple case for reading into a [Value::Array].
 pub struct ArrayDeserializer;
+impl ArrayDeserializerGeneric for ArrayDeserializer {
+    type Item = Value;
+    fn inner_type(type_: &Type) -> &Type {
+        type_.unwrap_array()
+    }
+    fn inner_value(items: Vec<Self::Item>) -> Value {
+        Value::Array(items)
+    }
+    fn item_mapping(value: Value) -> Value {
+        value
+    }
+}
 
 #[async_trait::async_trait]
-impl Deserializer for ArrayDeserializer {
+impl<T: ArrayDeserializerGeneric + 'static> Deserializer for T {
     async fn read_prefix<R: ClickhouseRead>(
         type_: &Type,
         reader: &mut R,
         state: &mut DeserializerState,
     ) -> Result<()> {
-        match type_ {
-            Type::Array(inner) => {
-                inner.deserialize_prefix(reader, state).await?;
-            }
-            _ => unimplemented!(),
-        }
-        Ok(())
+        Self::inner_type(type_)
+            .deserialize_prefix(reader, state)
+            .await
     }
 
     async fn read<R: ClickhouseRead>(
@@ -28,7 +48,6 @@ impl Deserializer for ArrayDeserializer {
         rows: usize,
         state: &mut DeserializerState,
     ) -> Result<Vec<Value>> {
-        let type_ = type_.unwrap_array();
         if rows == 0 {
             return Ok(vec![]);
         }
@@ -36,16 +55,17 @@ impl Deserializer for ArrayDeserializer {
         for _ in 0..rows {
             offsets.push(reader.read_u64_le().await?);
         }
-        let mut items = type_
+        let mut items = Self::inner_type(type_)
             .deserialize_column(reader, offsets[offsets.len() - 1] as usize, state)
             .await?
-            .into_iter();
+            .into_iter()
+            .map(Self::item_mapping);
         let mut out = Vec::with_capacity(rows);
         let mut read_offset = 0u64;
         for offset in offsets {
             let len = offset - read_offset;
             read_offset = offset;
-            out.push(Value::Array((&mut items).take(len as usize).collect()));
+            out.push(Self::inner_value((&mut items).take(len as usize).collect()));
         }
 
         Ok(out)
